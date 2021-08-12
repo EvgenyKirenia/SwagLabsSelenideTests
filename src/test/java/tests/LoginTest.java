43fb51c406524c4d8f5628a24558594e16082545
jon1807 <jon180789@gmail.com>
2021-08-12 20:37:57 +0300

    addAllureDependencies

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.TakesScreenshot;
 import testData.TestData;
 
 public class LoginTest extends TestData {
-    @Attachment(value = "Page screenshot", type = "image/png")
-    public byte[] saveScreenshot(byte[] screenShot) {
-        return screenShot;
-    }
+
     @Test
     public void logInTest() {
         projectData.getLogInPage().logInMethod();
